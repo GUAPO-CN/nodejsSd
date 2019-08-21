d77af012e27e317ad8f1000bb1278da70bdb84ef
guapo-cn <407919982@qq.com>
2019-08-21 17:51:52 +0800

    准备连接mongodb

--- config-project/js/index.js
+++ config-project/js/index.js
@@ -1,5 +1,4 @@
-let _eslint,editStr,_initEditor,submitText,creatXHR,xhrRequest;
-	var textarea2 = {
+	let textarea2 = {
 		"axios": {
 			"baseURL": function({
 				_req
@@ -61,102 +60,137 @@ let _eslint,editStr,_initEditor,submitText,creatXHR,xhrRequest;
 		document.getElementById('textarea').value= '';
 
 	//ace web编辑器 
-		_initEditor = () => {
-			// 1.初始化editor(）
-				var editor = ace.edit('editor');
-			//2.设置主题
-				editor.setTheme('ace/theme/monokai')
-				// editor.setTheme("ace/theme/twilight");
+		function AceEditor(params){
+			if(this instanceof AceEditor){
+				this.ace = params.ace
+				this.editStr = ''
+				this.callback = params.callback 
+				this.URL = params.url
 
-				editor.setOption('tabSize', 2)
-				editor.setOption('fontSize', 15)
-				editor.setOption('enableLiveAutocompletion', true)
-				editor.setOption('enableSnippets', true)
-				// editor.execCommand('find')//与ctrl+f功能一致
-				// editor.moveCursorTo(0, 0)//移动光标至第0行，第0列
-			//3.设置编辑语言
-				editor.getSession().setMode('ace/mode/javascript');
-
-				editor.clearSelection()
-				editor.getSession().setUseWorker(false)
-				editor.on('change', () => {
-					// console.log(editor.getValue(),'editor.getValue()');
-				})
-				editor.commands.addCommand({
-					name: 'save',
-					bindKey: {win: 'Ctrl-S', mac: 'Command-S'},
-					exec: () => {
-						// console.log(editor.getValue(),'editor.getValue()');
-						editStr = editor.getValue()
-						_eslint()
-						submitText(editStr)
+				if(typeof(this._initEditor) != 'function'){
+					AceEditor.prototype._initEditor = () => {
+						// 1.初始化editor(）
+							var editor = this.ace.edit('editor');
+						//2.设置主题
+							editor.setTheme('ace/theme/monokai')
+							// editor.setTheme("ace/theme/twilight");
+			
+							editor.setOption('tabSize', 2)
+							editor.setOption('fontSize', 15)
+							editor.setOption('enableLiveAutocompletion', true)
+							editor.setOption('enableSnippets', true)
+							// editor.execCommand('find')//与ctrl+f功能一致
+							// editor.moveCursorTo(0, 0)//移动光标至第0行，第0列
+						//3.设置编辑语言
+							editor.getSession().setMode('ace/mode/javascript');
+			
+							editor.clearSelection()
+							editor.getSession().setUseWorker(false)
+							editor.on('change', () => {
+								// console.log(editor.getValue(),'editor.getValue()');
+							})
+							editor.commands.addCommand({
+								name: 'save',
+								bindKey: {win: 'Ctrl-S', mac: 'Command-S'},
+								exec: () => {
+									// console.log(editor.getValue(),'editor.getValue()');
+									this.editStr = editor.getValue()
+									this._eslint()
+									this.submitText(this.editStr)
+								}
+							})
+							// editor.setValue(JSON.stringify(textarea2))
 					}
-				})
-				// editor.setValue(JSON.stringify(textarea2))
-		}
-		_eslint = () =>{
-			try {
-				const value = (new Function(`return ${editStr}`))() // eslint-disable-line
-				if (!value) {
-					alert('不能为空')
-					return
-				} else if (typeof value !== 'object') {
-					throw 'new Error()'
 				}
-			} catch (error) {
-				if (!/^http(s)?:\/\//.test(editStr)) {
-					console.log(error,'error');
-					alert('请输入对象格式')
-					return
-				}
-			}
-		}
-		submitText = (data) => {
-			let fd =new FormData();
-			fd.append('json',data)
-			let reqData = JSON.stringify({'json':data})
-			xhrRequest('http://localhost:5555/bhgConfig',reqData,(res)=>{
-				console.log(res,'res');
-			})
-		}
-		creatXHR = () => {
-			if(typeof XMLHttpRequest != 'undefined'){
-				return new XMLHttpRequest()
-			}else if(typeof ActiveXObject != 'undefined'){
-				if(typeof arguments.callee.activeXString != 'string'){
-					var version =  ['MSXML2.XMLHttp.6.0','MSXML2.XMLHttp.3.0' ,'MSXML2.XMLHttp'], i,len;
-						for(i=0,len=versions.length; i<len;i++){
-							try{
-								new ActiveXObject(versions[i]);
-								arguments.callee.activeXString = versions[i];
-								break;
+				if(typeof(this._eslint) != 'function'){
+					AceEditor.prototype._eslint = () =>{
+						try {
+							const value = (new Function(`return ${this.editStr}`))() // eslint-disable-line 将editStr 动态解析执行字符串
+							if (!value) {
+								alert('不能为空')
+								return
+							} else if (typeof value !== 'object') {
+								throw 'new Error()'
 							}
-							catch(ex){
-								// jump
+						} catch (error) {
+							if (!/^http(s)?:\/\//.test(this.editStr)) {
+								console.log(error,'error');
+								alert('请输入对象格式')
+								return
 							}
 						}
-					return new ActiveXObject(arguments.callee.activeXString);
+					}
+				}
+				if(typeof(this.submitText) != 'function'){
+					AceEditor.prototype.submitText = (data) => {
+						let reqData = JSON.stringify({'json':data})
+						let xhr = new XHR();
+						xhr.xhrRequest(this.URL,reqData,(res)=>{
+							// console.log(res,'res');
+							this.callback(res)
+						})
+					}
+				}
+			}
+		}
+
+		function XHR(){
+			if(this instanceof XHR){
+				if(typeof(this.creatXHR) != 'function'){
+					XHR.prototype.creatXHR = () => {
+						if(typeof XMLHttpRequest != 'undefined'){
+							return new XMLHttpRequest()
+						}else if(typeof ActiveXObject != 'undefined'){
+							if(typeof arguments.callee.activeXString != 'string'){
+								var versions =  ['MSXML2.XMLHttp.6.0','MSXML2.XMLHttp.3.0' ,'MSXML2.XMLHttp'], i,len;
+									for(i=0,len=versions.length; i<len;i++){
+										try{
+											new ActiveXObject(versions[i]);
+											arguments.callee.activeXString = versions[i];
+											break;
+										}
+										catch(ex){
+											// jump
+										}
+									}
+								return new ActiveXObject(arguments.callee.activeXString);
+							}
+						}else{
+							throw new Error('No XHR object available.') 
+						}
+					}
+				}
+				if(typeof(this.xhrRequest) != 'function'){
+					XHR.prototype.xhrRequest = (url,body,callback) => {
+						let xhr = this.creatXHR();
+						xhr.onreadystatechange = ()=> {
+							if(xhr.readyState == 4) {
+								if(xhr.status >= 200 && xhr.status < 300 || xhr.status == 304){//200 表示相应成功 304 表示缓存中存在请求的资源
+									let str = xhr.responseText;
+									callback(str);
+								}
+								else{
+									return 'request is unsucessful '+xhr.status;
+								}
+							}
+						}
+						xhr.open('post',url,true)
+						// xhr.setRequestHeader('Content-Type','application/x-www-form-urlencoded')
+						xhr.setRequestHeader('Content-Type','application/json')
+						xhr.send(body)
+					}
 				}
 			}else{
-				throw new Error('No XHR object available.') 
+				throw Error('This function is requires new! ep: new XHR()');
 			}
 		}
-		xhrRequest = (url,body,callback) => {
-			let xhr = creatXHR();
-			xhr.onreadyStateChange = ()=> {
-				if(xhr.readyStatus == 4) {
-					if(xhr.status >= 200 && xhr.status < 300 || xhr.status == 304){//200 表示相应成功 304 表示缓存中存在请求的资源
-						let str = xhr.responseText;
-						callback(str);
-					}
-					else{
-						return 'request is unsucessful '+xhr.status;
-					}
-				}
-			}
-			xhr.open('post',url,true)
-			// xhr.setRequestHeader('Content-Type','application/x-www-form-urlencoded')
-			xhr.setRequestHeader('Content-Type','application/json')
-			xhr.send(body)
-		}
-		_initEditor();
+
+		const aceEditor = new AceEditor({
+			'ace':ace,
+			'url':'http://localhost:5555/addConfig',
+			'callback':(res) => {
+				document.getElementById('id_iframe').contentWindow.document.body.innerHTML = res;
+				console.log(res,'ressss');
+			},
+		});
+		aceEditor._initEditor();
--- config-project/server.js
+++ config-project/server.js
@@ -2,64 +2,66 @@ const http = require('http');
 const path = require('path');
 const express =require('express');
 const bodyParser = require('body-parser');
-
 const unlencoderParser = bodyParser.urlencoded({extended:false});
+var dataObj;
+
 const app = express();
-// app.use(bodyParser.json())
 app.use('/js',express.static('js'))
-var  dataObj;
+// app.use(bodyParser.json())
 
 //设置所有请求的 跨域请求
-app.all('*',function(req,res,next){
-	res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild");
-    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
-    res.header("X-Powered-By",' 3.2.1')
-    res.header("Content-Type", "application/json;charset=utf-8");
-    next();
-})
+	app.all('*',function(req,res,next){
+		res.header("Access-Control-Allow-Origin", "*");
+			res.header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild");
+			res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+			res.header("X-Powered-By",' 3.2.1')
+			res.header("Content-Type", "application/json;charset=utf-8");
+			next();
+	})
 
-app.get('/',(req,res) => {
-	res.header("Content-Type", "text/html; charset=utf-8");
-	res.sendFile(__dirname +'/index.html')
-})
+//获取首页 index.html展示
+	app.get('/',(req,res) => {
+		res.header("Content-Type", "text/html; charset=utf-8");
+		res.sendFile(__dirname +'/index.html')
+	})
 
-app.post('/bhgConfig',unlencoderParser,(req,res,next) => {
-	const _req = req;
-	const headersType = req.headers['content-type'];
-	// console.log(headersType,'');
-	if( headersType == 'application/x-www-form-urlencoded'){
-		//处理form-data格式的数据 
-			dataObj = eval('('+_req.body.json+')')
-			initObj(dataObj,_req)
-			var response = {
-				'username':_req.body.userName,
-				'textarea':dataObj,
-			}
-			res.end(JSON.stringify(response))
-			return;
-	}else if( headersType == 'application/json'){
-		//处理payload格式的数据 用on data end
-			let str = ''
-			_req.on('data',(chunk)=>{
-				str = chunk.toString();
-				dataObj = eval('('+JSON.parse(str).json+')')
+//新增 配置
+	app.post('/addConfig',unlencoderParser,(req,res,next) => {
+		const _req = req;
+		const headersType = req.headers['content-type'];
+		// console.log(headersType,'');
+		if( headersType == 'application/x-www-form-urlencoded'){
+			//处理form-data格式的数据 
+				dataObj = eval('('+_req.body.json+')')
 				initObj(dataObj,_req)
-			})
-			_req.on('end',()=>{
 				var response = {
 					'username':_req.body.userName,
 					'textarea':dataObj,
 				}
 				res.end(JSON.stringify(response))
-			})
-			return;
-	}
+				return;
+		}else if( headersType == 'application/json'){
+			//处理payload格式的数据 用on data end
+				let str = ''
+				_req.on('data',(chunk)=>{
+					str = chunk.toString();
+					dataObj = eval('('+JSON.parse(str).json+')')
+					initObj(dataObj,_req)
+				})
+				_req.on('end',()=>{
+					var response = {
+						'username':_req.body.userName,
+						'textarea':dataObj,
+					}
+					res.end(JSON.stringify(response))
+				})
+				return;
+		}
+	})
 
-})
 
 let server = app.listen(5555, () => {
-    let host = server.address().address;
+	let host = server.address().address;
 	let port = server.address().port;
 	console.log("应用实例，访问地址为 http://%s:%s", host, port)
 })
@@ -80,3 +82,13 @@ function initObj(obj,_req){
 		}
 	}
 }
+
+
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/runoob";
+ 
+MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
+  if (err) throw err;
+  console.log("数据库已创建!");
+  db.close();
+});
